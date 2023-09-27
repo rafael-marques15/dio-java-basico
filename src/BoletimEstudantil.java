e6aa8a8bc8fc5aead159c3b296271997cf81e1ed
Rafael <rafaelsccp00@gmail.com>
2023-09-27 18:42:38 -0300

    atualizacao

--- src/BoletimEstudantil.java
+++ src/BoletimEstudantil.java
@@ -1,6 +1,6 @@
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 5
+        int mediaFinal = 7;
         if (mediaFinal < 6)
             System.out.println("Reprovado");
         else if (mediaFinal == 6)
