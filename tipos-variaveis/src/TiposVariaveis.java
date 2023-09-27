9c0f2986226489257d80f82d3a4145a650755464
Rafael <rafaelsccp00@gmail.com>
2023-09-27 19:52:35 -0300

    exemplos de tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    byte idade = 123;
+    short ano = 2023;
+    int numero = 12345;
+    long cpf = 1234567890L;
+    float pi = 3.14f;
+    double altura = 1.80;
 }
