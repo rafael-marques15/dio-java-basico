269ed3588fe7ce85978b4ae9c72c9dd700d88ada
Rafael <rafaelsccp00@gmail.com>
2023-10-02 14:34:50 -0300

    operadores unitarios

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,7 +1,9 @@
 public class Operadores {
-
     public static void main(String[] args) {
         String nomeCompleto = "Linguagem" + "Java";
         System.out.println(nomeCompleto);
+
+        int numero = 5;
+        System.out.println(++numero);
     }
 }
