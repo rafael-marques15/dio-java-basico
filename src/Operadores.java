387cf1a8825942b9c5b0f799a5955e121a2fee2f
Rafael <rafaelsccp00@gmail.com>
2023-10-02 14:39:46 -0300

    Operador unario de incremento

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,8 +1,11 @@
 public class Operadores {
     public static void main(String[] args) {
+
+        // Concatenação de valores
         String nomeCompleto = "Linguagem" + "Java";
         System.out.println(nomeCompleto);
 
+        // Operador unário, o ex baixo é:incremento
         int numero = 5;
         System.out.println(++numero);
     }
