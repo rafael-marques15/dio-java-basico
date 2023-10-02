4c5d1833ea7461c28b004cdc8d86205a9fba823a
Rafael <rafaelsccp00@gmail.com>
2023-10-02 17:37:01 -0300

    operador ternário

--- src/Operadores.java
+++ src/Operadores.java
@@ -2,11 +2,20 @@ public class Operadores {
     public static void main(String[] args) {
 
         // Concatenação de valores
-        String nomeCompleto = "Linguagem" + "Java";
+        String nomeCompleto = "Linguagem " + "Java";
         System.out.println(nomeCompleto);
 
         // Operador unário, o ex baixo é:incremento
         int numero = 5;
         System.out.println(++numero);
+
+        // Operador ternário
+
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
     }
 }
