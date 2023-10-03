b2db2af2479723c1ae77e7b6a8ace7eeade339e5
Rafael <rafaelsccp00@gmail.com>
2023-10-03 12:16:25 -0300

    Métodos

---
+++ src/Metodos.java
@@ -0,0 +1,11 @@
+public class Metodos {
+
+    // Método privado - é visivel somente para essa classe
+    private void metodoPrivado() {
+    }
+
+    // Método publico - é visivel por todas as classes]
+    public void validar() {
+    }
+
+}
