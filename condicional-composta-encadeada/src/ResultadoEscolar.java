378989de1eee6baa9b8d993273a3e30020b03b4b
Rafael <rafaelsccp00@gmail.com>
2023-10-04 12:02:08 -0300

    condicional composta e encadeada

--- condicional-composta-encadeada/src/ResultadoEscolar.java
+++ condicional-composta-encadeada/src/ResultadoEscolar.java
@@ -5,6 +5,10 @@ public class ResultadoEscolar {
 
         if (nota >= 7)
             System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de recuperação");
+
         else
             System.out.println("Reprovado");
 
